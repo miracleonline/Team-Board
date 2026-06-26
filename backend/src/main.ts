f6ab8d526503ffa2b4f6a261e1203bf046d5b0c2
Miracle Nwabuisi <Thebluelogic@gmail.com>
2026-06-27 00:35:24 +0100

    fix: updated cors url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://team-board-olive.vercel.app/login',
     credentials: true,
   });
 
